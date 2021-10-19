e9f8ece4df924a6648c4adef4495e2de55d8a039
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-19 17:08:19 -0300

    Requisito 3, Criando componente Header e renderizando em varios endpoints

---
+++ src/components/Header.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      userName: 'user',
+    };
+
+    this.setLoading = this.setLoading.bind(this);
+    this.updateUserName = this.updateUserName.bind(this);
+  }
+
+  componentDidMount() {
+    const { userName } = this.state;
+    if (userName === 'user'){
+      this.updateUserName();
+    }
+  }
+
+  setLoading(value) {
+    this.setState({ loading: value });
+  }
+
+  async updateUserName() {
+    this.setLoading(true);
+
+    const user = await getUser();
+    const userName = user.name;
+    this.setState({ userName });
+
+    this.setLoading(false);
+  }
+
+  render() {
+    const { userName, loading } = this.state;
+    return (
+      <div>
+        {loading ? <Loading /> : (
+          <header data-testid="header-component">
+            <p>Trybetunes logo</p>
+            <div>
+              <span
+                data-testid="header-user-name"
+              >
+                {userName}
+              </span>
+            </div>
+          </header>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Header;
