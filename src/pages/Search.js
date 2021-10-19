9f5f2738900e2a5d91cfb099a6e6852a87587139
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-19 19:02:32 -0300

    Requisito 5, Criando form do search para pesquisar artista

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,66 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistInput: '',
+      buttonIsDisable: true,
+    };
+
+    this.validateButton = this.validateButton.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value },
+      () => {
+        this.setState({ buttonIsDisable: this.validateButton() });
+      });
+  }
+
+  validateButton() {
+    const { artistInput } = this.state;
+    const minCharacteres = 2;
+    let buttonShouldBeDisable = true;
+
+    if (artistInput.length >= minCharacteres) {
+      buttonShouldBeDisable = false;
+    }
+
+    return buttonShouldBeDisable;
+  }
+
   render() {
+    const { artistInput, buttonIsDisable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <div>
+            <label htmlFor="artist-input">
+              Artista
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                name="artistInput"
+                id="artist-input"
+                placeholder="nome do artista"
+                value={ artistInput }
+                onChange={ this.handleChange }
+              />
+            </label>
+          </div>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ buttonIsDisable }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
